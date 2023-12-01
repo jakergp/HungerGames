5f6c8925c83bdd4501500535d7e3e4056580279e
Javier Arias <javierariashe@gmail.com>
2023-11-30 18:58:43 -0600

    Clases creadas

--- Main/Creador.java
+++ Main/Creador.java
@@ -1,5 +1,18 @@
 package Main;
 
-public class Creador {
+import Tributos.*;
+import java.util.ArrayList;
 
+public class Creador {
+    public void registrarListaTributos() {
+        //TODO: Registrar lista de 24 tributos y guardarlos en un archivo de objetos.
+    }
+
+    public void registrarTributo() {
+       //TODO: El usuario podrá ingresar los datos de un tributo y se registrará.
+    }
+
+    public ArrayList<Tributo> leerListaTributos() {
+        //TODO: Leer archivo de objetos que devuelva la lista de los tributos registrados.
+    }
 }
