7125e8f35cc90011a06822caaee79b883f07d7a8
Leslie Jaramillo <leslie.jarod1@gmail.com>
2023-12-02 22:50:39 -0600

    Eventos

--- Evento/Desastre.java
+++ Evento/Desastre.java
@@ -1,35 +1,52 @@
 package Evento;
 
 import java.util.ArrayList;
-
+import JuegosDelHambre.Juegos;
+import java.util.Random;
 import Tributos.Tributo;
 
 public class Desastre implements Evento{
     private String nombre;
-    private ArrayList<Tributo> tributos;
-
-    public Desastre(String nombre, ArrayList<Tributo> listaTributos) {
+    private ArrayList<Tributo> tributos=new ArrayList<>();
+    private ArrayList<Tributo> muertos=new ArrayList<>();
+    Juegos observador;
+    
+    public Desastre(String nombre, Juegos observador) {
+        Random = new Random();
         this.nombre = nombre;
-            
+        ArrayList<Tributo> listaT=observador.getTributos();
+        for (int i=0; i<5; i++){
+          int index=random.nextInt(listaT.size());
+          this.tributos.add(listaT.get(index));
+          listaT.remove(index);
+      }
     }
 
     @Override
     public void iniciar() {
-        //TODO: Iniciar evento
+      for (Tributo tributo : tributos) {
+        int agilidad=tributo.getAgilidad()*0.1;
+        if (Math.random()<agilidad){
+          tributo.herida();
+        }
+        else {
+          observador.notificarMuerte(tributo, this); 
+          this.muertos.add(tributo);
+        }
+      }
     }
 
     @Override
     public void mostrar() {
-        //TODO: Mostrar Evento
+      System.out.println("Ocurrió un desastre" +this.nombre);
+      System.out.println("Tributos que no sobrevivieron: ");
+      for(Tributo tributo: muertos){
+        System.out.println(tributo.getNombre());
+      }
+        
     }
 
     public String getNombre() {
         return nombre;
     }
-
-    @Override
-    public String toString(){
-        return " " + this.nombre;
-    }
-
 }
